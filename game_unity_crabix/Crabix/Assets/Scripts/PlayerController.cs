5ae0e22c4f8a8a788f4ea57d73bdc7ba72b143f1
Cengiz <cengiz.kasmeroglu@kasisoft.com>
2021-01-07 16:07:38 +0100

    new movement system. add a idle status and a movement animation

---
+++ game_unity_crabix/Crabix/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    private Vector3 movementVector;
+
+    private Rigidbody rBody;
+    private Animator anim;
+
+    private void Awake()
+    {
+        rBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        movementVector = new Vector3(h, 0, v);
+
+        if (movementVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementVector), rotationSpeed);
+        }
+        
+        
+
+        Move();
+    }
+
+    private void Move()
+    {
+        movementVector = movementVector.normalized * moveSpeed * Time.deltaTime;
+        rBody.MovePosition(transform.position + movementVector);
+
+        bool isWalking = movementVector.x != 0f || movementVector.z != 0f;
+        anim.SetBool("isWalking", isWalking);
+
+
+
+    }
+
+}
